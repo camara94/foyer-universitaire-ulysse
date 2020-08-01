13796794f7f07b06c61603f0cbaa01b080942bd5
camara94 <ldamaro98@gmail.com>
2020-08-01 08:17:37 +0200

    service d'enregistrement

---
+++ service-register/src/test/java/tn/uud/register/ServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.uud.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
