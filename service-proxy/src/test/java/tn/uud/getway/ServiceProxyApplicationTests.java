a65b98003e0028080a57098726ef1a20d3420d19
camara94 <ldamaro98@gmail.com>
2020-08-01 08:20:25 +0200

    service proxy

---
+++ service-proxy/src/test/java/tn/uud/getway/ServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.uud.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
